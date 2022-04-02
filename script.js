5acdc1f654f43e55ae7562a2b7d68531abf3ee2c
Chris Porter <cap8095@gmail.com>
2022-04-02 14:22:53 -0400

    got the rows lined up and working on the coloring

--- script.js
+++ script.js
@@ -6,4 +6,19 @@ function displayDate() {
   dateDiv.textContent = moment;
 }
 console.log(moment);
+
+let t = new Date();
+let hour = t.getHours();
+const cols = [...document.querySelectorAll("h4")];
+cols.forEach((h) => (h.className = ""));
+cols.forEach((h, i) => {
+  if (i + 9 < hour) {
+    h.className = "past";
+  } else if (i + 9 > hour) {
+    h.className = "future";
+  } else {
+    h.className = "present";
+  }
+});
+
 displayDate();
